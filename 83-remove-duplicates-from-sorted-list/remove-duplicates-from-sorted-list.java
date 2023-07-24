3c968b5176d3056ae5906da968c4e436038e3815
AV <92280537+adivaste@users.noreply.github.com>
2023-07-24 23:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (91.64%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -10,25 +10,17 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        
-        // Approach 1 : Get the value of next node, if its same you
-        // current then remove the next node, if not same otherwise go next
-
-        ListNode temp = head;
-
-        while(temp != null){
-            // Check next value is same as curr, before that check next is present
-            // or not. If not same then go next
-            if((temp.next != null) && (temp.val == temp.next.val)){
-                temp.next = temp.next.next;
-            }
-            else{
-                temp = temp.next;
-            }
+        if (head == null || head.next == null) {
+            return head;
         }
-
+    
+        if (head.val == head.next.val) {
+            head = deleteDuplicates(head.next);
+        } else {
+            head.next = deleteDuplicates(head.next);
+        }
+    
         return head;
+}
 
-        
-    }
 }
