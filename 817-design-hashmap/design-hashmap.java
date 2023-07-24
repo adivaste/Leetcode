96af564514058cf07b8910b2f3f6202fbe6db362
AV <92280537+adivaste@users.noreply.github.com>
2023-07-24 22:28:09 +0530

    Time: 44 ms (28.10%) | Memory: 56.4 MB (6.22%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -2,26 +2,59 @@ class MyHashMap {
 
 
     private int size;
-    private int[] hashmap;
+    private LinkedList<Pair<Integer, Integer>>[] hashmap;
 
     public MyHashMap() {
-        this.size = 1000000 + 1;
-        this.hashmap = new int[this.size];
-        for(int i=0; i<this.size; i++){
-            this.hashmap[i] = -1;
+        this.size = 100000 + 1;
+        this.hashmap = new LinkedList[this.size];
+
+        for (int i = 0; i < this.size; i++) {
+            this.hashmap[i] = new LinkedList<>();
         }
-    }   
-    
+    }
+
+    public int getIndex(int key) {
+        return key % this.size;
+    }
+
+    public int getInnerIndex(int key) {
+        int index = getIndex(key);
+        for (int i = 0; i < hashmap[index].size(); i++) {
+            if (hashmap[index].get(i).getKey().equals(key)) return i;
+        }
+        return -1;
+    }
+
     public void put(int key, int value) {
-        this.hashmap[key] = value;
+        Pair<Integer, Integer> temp = new Pair(key, value);
+        int index = getIndex(key);
+        int innerIndex = getInnerIndex(key);
+
+        if (innerIndex != -1) {
+            this.hashmap[index].set(innerIndex, temp); // Update the existing key-value pair
+        } else {
+            this.hashmap[index].add(temp); // Add a new key-value pair
+        }
     }
-    
+
     public int get(int key) {
-        return this.hashmap[key];
+        int index = getIndex(key);
+        int innerIndex = getInnerIndex(key);
+
+        if (innerIndex != -1) {
+            return this.hashmap[index].get(innerIndex).getValue(); // Return the value if key exists
+        }
+
+        return -1; // Key not found, return -1
     }
-    
+
     public void remove(int key) {
-        this.hashmap[key] = -1;
+        int index = getIndex(key);
+        int innerIndex = getInnerIndex(key);
+
+        if (innerIndex != -1) {
+            this.hashmap[index].remove(innerIndex); // Remove the key-value pair if key exists
+        }
     }
 }
 
