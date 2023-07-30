6f6fdb8499c4660f0b0d65baee02e14d164c4a7d
AV <92280537+adivaste@users.noreply.github.com>
2023-07-31 00:58:43 +0530

    Time: 1071 ms (5.39%) | Memory: 48.4 MB (58.66%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        // :: A1 - Brutforce
+        int size = nums.length;
+        for(int i=0; i<size; i++){
+            int count = 0;
+            for(int j=i; j<size; j++){
+                if(nums[i] == nums[j]) count++;
+            }
+            if (count > size/2) return nums[i];
+        }
+        return -1;
+
+    }
+}
