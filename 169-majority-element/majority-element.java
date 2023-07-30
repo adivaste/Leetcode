acd6765cf67de54d1effd0bfd172c9d27d0bc113
AV <92280537+adivaste@users.noreply.github.com>
2023-07-31 01:09:27 +0530

    Time: 13 ms (30.93%) | Memory: 46.7 MB (88.35%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,34 @@
+
 class Solution {
     public int majorityElement(int[] nums) {
-        
-        // :: A1 - Brutforce
-        int size = nums.length;
-        for(int i=0; i<size; i++){
-            int count = 0;
-            for(int j=i; j<size; j++){
-                if(nums[i] == nums[j]) count++;
-            }
-            if (count > size/2) return nums[i];
-        }
-        return -1;
 
+        // A2 - HashMap
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int size = nums.length;
+        for (int i = 0; i < size; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+        int max = 0;
+        int answer = -1;
+        for (int value : map.values()) {
+            if (value > max) {
+                max = value;
+            }
+        }
+
+        for (int key : map.keySet()) {
+            if (map.get(key) == max) {
+                answer = key;
+                break; // No need to continue once we find the majority element
+            }
+        }
+
+        return answer;
     }
 }
