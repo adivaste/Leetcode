635115167decba2b03bc7e9180eb9e319bc05510
AV <92280537+adivaste@users.noreply.github.com>
2023-07-31 20:50:43 +0530

    Time: 70 ms (92.03%) | Memory: 45.3 MB (9.57%) - LeetSync

--- 1775-design-an-ordered-stream/design-an-ordered-stream.java
+++ 1775-design-an-ordered-stream/design-an-ordered-stream.java
@@ -2,21 +2,18 @@ class OrderedStream {
 
     private HashMap<Integer, String> map;
     private int pointer;
-    private int max;
 
     public OrderedStream(int n) {
         this.map = new HashMap<>();
         this.pointer = 1;
-        this.max = 0;
     }
     
     public List<String> insert(int idKey, String value) {
         map.put(idKey, value);
-        this.max = Math.max(this.max, idKey);
 
         List<String> answer = new ArrayList<String>();
 
-        while(this.pointer <= this.max && map.get(this.pointer) != null){
+        while(this.pointer <= this.map.size() && map.get(this.pointer) != null){
             String mapVal = map.get(this.pointer);
             answer.add(mapVal);
             this.pointer++;
