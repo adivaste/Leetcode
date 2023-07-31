7c2ab067b589c19c06787dcfb5c8acd2b30c70ae
AV <92280537+adivaste@users.noreply.github.com>
2023-07-31 18:32:06 +0530

    Time: 162 ms (5.60%) | Memory: 44.5 MB (7.24%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -4,23 +4,21 @@ class Solution {
         // :: A1 : Brutforce 
         int size = nums.length;
         int[] answer = new int[size];
-
-        for(int i=0; i<size; i++){
-
-            int leftSum = 0;
-            for(int j=0; j<i; j++){
-                leftSum += nums[j];
-            }
-
-            int rightSum = 0;
-            for(int k=i+1; k<size; k++){
-                rightSum += nums[k];
-            }
-
-            answer[i] = Math.abs(leftSum-rightSum);
+        int totalSum = 0;
+        for(int num : nums){
+            totalSum += num;
         }
 
+        int leftSum = 0;
+        for(int i=0; i<size; i++){
+            System.out.println("Left Sum : " + leftSum);
+            System.out.println("Total Sum : " + totalSum);
+            System.out.println("Answer : " + (totalSum - (nums[i] + leftSum)));
+            int ss = totalSum - (nums[i] + 2*leftSum);
+            answer[i] = Math.abs(ss);
+            leftSum = leftSum + nums[i];
+            System.out.println("Left sum after : "+leftSum);
+        }
         return answer;
-
     }
 }
