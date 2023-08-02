ec1d04446609a6fb88d84310c4d4a5e966411f81
AV <92280537+adivaste@users.noreply.github.com>
2023-08-02 18:40:03 +0530

    Time: 468 ms (24.86%) | Memory: 50.8 MB (68.56%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,54 +3,37 @@ class Solution {
     // Answer to store
     List<List<Integer>> answer = new LinkedList<List<Integer>>();
 
-    public void twoSum(int[] nums, int target, int i, int j){
-        while(i < j){
-            int result = nums[i] + nums[j];
-
-            if (result == target){
-                // Also have to remove duplicates
-                
-                while(i < j && nums[i] == nums[i+1]) i++;
-                while(i < j && nums[j] == nums[j-1]) j--;
-
-                LinkedList<Integer> temp = new LinkedList<>();
-                temp.add(-target);
-                temp.add(nums[i]);
-                temp.add(nums[j]);
-                answer.add(temp);
-                System.out.println(temp);
-
-                i++;
-                j--;
-            }
-            else if (result  < target) i++;
-            else j--;
-        }
-    }
     public List<List<Integer>> threeSum(int[] nums) {
-
-        // Size
-        int size = nums.length;
-
-        // Base case
-        if (size < 3) return new LinkedList<List<Integer>>();
-
-        // Step 1 : Sort the array
+        int target = 0;
         Arrays.sort(nums);
 
-        // Step 2 : 
-        for(int i=0; i<size-2; i++){
-            if (i > 0 && nums[i] == nums[i-1]){
-                continue;
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+
+        for(int i=0; i<nums.length; i++){
+            int j = i+1;
+            int k = nums.length - 1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                }
+                else if (sum < target) {
+                    j++;
+                } 
+                else {
+                    k--;
+                }
             }
-
-            int n1 = nums[i];
-            int target = -n1;
-
-            // It will find n2 and n3 
-            twoSum(nums, target, i+1, size-1);
-
         }
-    return answer;
+
+        output.addAll(s);
+        return output;
+
+
+
     }
 }
