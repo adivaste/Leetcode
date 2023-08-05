8584394d58664c5496d4213cac8b35611384b79b
AV <92280537+adivaste@users.noreply.github.com>
2023-08-05 22:35:00 +0530

    Time: 2 ms (5.46%) | Memory: 41.3 MB (29.12%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int numberOfTimesArraySorted(int[] arr){
+        if (arr.length == 1) return 0;
+
+        // For array find index of minimum element that answer
+        int size = arr.length;
+        int left = 0;
+        int right = size - 1;
+
+        int minIndex = -1;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            int prev = (mid+1)%size;
+            int next = (mid-1+size)%size;
+
+            if (arr[mid] < arr[prev] && arr[next] > arr[mid]){
+                 minIndex = mid;
+                 break;
+            }
+            else if (arr[mid] < arr[right]) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return minIndex;
+    }
+
+    public int binarySearch(int[] arr, int left, int right, int target){
+        if (left <= right) {
+            int mid = left + (right-left)/2;
+
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] > target) return binarySearch(arr, left, mid-1, target);
+            else return binarySearch(arr, mid+1, right, target);
+        }
+        return -1;
+    }
+
+    public int search(int[] arr, int target) {
+        int minIndex = numberOfTimesArraySorted(arr);
+        System.out.println(minIndex);
+        int index1 = binarySearch(arr, 0, minIndex-1, target);
+        int index2 = binarySearch(arr, minIndex, arr.length-1, target);
+
+        if (index1 == -1) return index2;
+        return index1;
+    }
+}
