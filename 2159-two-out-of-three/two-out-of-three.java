35f29b7cab590590d993126d6aa3ff5bd54e1b77
AV <92280537+adivaste@users.noreply.github.com>
2023-08-18 18:48:23 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (69.60%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.java
+++ 2159-two-out-of-three/two-out-of-three.java
@@ -1,6 +1,8 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         List<Integer> list = new ArrayList<>();
+
+
         int count[][] = new int[3][101];
         for(int n : nums1)
             count[0][n] = 1;
@@ -13,6 +15,7 @@ class Solution {
             if(count[0][i] + count[1][i] + count[2][i] > 1)
                 list.add(i);
         }
+        
         return list;
     }
 }
