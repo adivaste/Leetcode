51ef5e9901f1db1162d2361e11235ced77dda404
Aditya Vaste <92280537+adivaste@users.noreply.github.com>
2023-08-31 19:34:52 +0530

    Time: 2 ms (85.74%) | Memory: 40.3 MB (90.02%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,45 +1,32 @@
 class Solution {
-    public boolean isValid(String expression) {
-        Stack<Character> s = new Stack<>();
-        HashMap<Character, Character> h = new HashMap<>();
-        h.put(')', '(');
-        h.put(']', '[');
-        h.put('}', '{');
+    public boolean isValid(String s) {
+        if (s.length()%2 == 1) return false;
+        
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('}', '{');
+        map.put(')', '(');
+        map.put(']', '[');
 
-        for(int i=0; i<expression.length(); i++){
-
-            // Get character
-            char c = expression.charAt(i);
-
-            // If it is opening bracket then push to stack
-            if (h.containsValue(c)){
-                s.push(c);
-            }
+        for(char ch : s.toCharArray()){
             
-            // If it is closing bracket, get its opening pair
-            // from map, and match with stack top
-            // Matched :: pop()
-            // No matched :: false | not balanced expression 
-            else if (h.containsKey(c) && s.size() != 0){
-                if (h.get(c) == s.peek()){
-                    s.pop();
+            // If opening bracket is there
+            if (ch == '{' || ch == '[' || ch == '('){
+                stack.push(ch);
+            }
+            else {
+                // Closing Bracket
+                if (!stack.isEmpty()){
+                    char top = stack.pop();
+                    if (map.get(ch) != top) return false;
                 }
                 else{
                     return false;
                 }
             }
-            // Stack is empty, still bracket in expression, means thats
-            // extra part
-            else if (h.containsKey(c) && s.size() == 0){
-                return false;
-            }
         }
-
-        // Expression ended stil stack has something then return false
-        // otherwise true
-        if (s.empty()){
-            return true;
-        }
+        
+        if (stack.isEmpty()) return true;
         return false;
     }
 }
