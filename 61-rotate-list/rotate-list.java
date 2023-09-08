75b75e1bf941cca714405dcaf1ce5fee5f02becd
Aditya Vaste <92280537+adivaste@users.noreply.github.com>
2023-09-08 17:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (34.24%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -10,36 +10,44 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) {
-            return head; // No rotation needed
-        }
-
-        // Calculate the length of the linked list
-        int length = 1;
-        ListNode tail = head;
-        while (tail.next != null) {
-            tail = tail.next;
+        
+        if(head == null || head.next == null || k == 0) return head;
+        
+        // Calculate the size of linked list
+        int length = 0;
+        ListNode temp = head;
+        while(temp != null){
             length++;
+            temp = temp.next;
         }
 
-        // Perform k right rotations
-        for (int i = 0; i < k % length; i++) {
-            ListNode current = head;
-            ListNode prev = null;
+        // Optimise using length
+        k = k % length;
+        
+        // If k is 0, no rotation needed
+        if (k == 0) return head;
 
-            // Move to the second-to-last node
-            while (current.next != null) {
-                prev = current;
-                current = current.next;
-            }
+        // Nodes to traverse
+        int nodesToTraverse = length - k;
 
-            // Move the last node to the front
-            current.next = head;
-            head = current;
-            prev.next = null;
+        // Use dummy node
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        temp = dummy;
+        
+        while(nodesToTraverse != 0){
+            temp = temp.next;
+            nodesToTraverse--;
         }
 
-        return head;
+        ListNode tempHead = temp.next;
+        temp.next = null;
+
+        ListNode curr = tempHead;
+        while(curr.next != null){
+            curr = curr.next;
+        }
+        curr.next = dummy.next;
+        return tempHead;
     }
-
 }
