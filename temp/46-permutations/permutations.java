f32758190fd1354bdac6914013f92b4675a35135
Aditya Vaste <92280537+adivaste@users.noreply.github.com>
2023-12-21 11:16:06 +0530

    Time: 12 ms (6.85%) | Memory: 45.7 MB (8.64%) - LeetSync

--- temp/46-permutations/permutations.java
+++ temp/46-permutations/permutations.java
@@ -1,31 +1,36 @@
 class Solution {
+    
     public List<List<Integer>> permute(int[] nums) {
-        int size = nums.length;
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> answer = new ArrayList<Integer>();
-        boolean[] checks = new boolean[size];
+        Set<List<Integer>> set = new HashSet<List<Integer>>();
 
-        findPermute(nums, result, answer, checks);
+        findPermute(nums, result, 0, set);
 
         return result;
     }
 
-    public void findPermute(int[] nums, List<List<Integer>> result, List<Integer> answer, boolean[] checks){
-        if (answer.size() == nums.length){
-            List<Integer> temp = new ArrayList<>(answer);
-            result.add(temp);
+    public void findPermute(int[] nums, List<List<Integer>> result, int curr, Set<List<Integer>> set){
+        
+        if(curr == nums.length){
+            List<Integer> temp = new ArrayList<>();
+            for (int num : nums) temp.add(num);
+            if (!set.contains(temp)){
+                set.add(temp);
+                result.add(temp);
+            }
             return;
         }
 
         for(int i=0; i<nums.length; i++){
-            if (!checks[i]){
-                answer.add(nums[i]);
-                checks[i] = true;
-                findPermute(nums, result, answer, checks);
-                checks[i] = false;
-                answer.remove(answer.size()-1);
-            }
+            swap(nums, curr, i);
+            findPermute(nums, result, curr+1, set);
+            swap(nums, curr, i);
         }
     }
 
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
